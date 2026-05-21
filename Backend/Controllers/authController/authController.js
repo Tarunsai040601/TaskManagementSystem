7316d17082ecdb411145b2341dfbf553f6fd902c
Madhari Tarunsai <saitarunsai04062002@gmail.com>
2026-05-21 22:16:44 +0530

    added

--- Backend/Controllers/authController/authController.js
+++ Backend/Controllers/authController/authController.js
@@ -36,9 +36,38 @@ const registerController = async(req,res) => {
   }
 };
 // login controller
-const loginController = () => {
+const loginController = async(req,res) => {
   try {
-  } catch (error) {}
+    const {  email, password } = req.body;
+    if ( !email || !password) {
+      return res.status(404).json({
+        status: false,
+        message: "all fields are required",
+      });
+      const user=await authSchema.find({email});
+      if(!user){
+        return res.status(404).json({
+        status: false,
+        message: `employee not  exist with this email :${user.email}`,
+      });
+      }
+      const passwordCompare=await bcrypt.compare(password,user.password);
+      const createUser=await authSchema.create({name,password:passwordHashing,email,role})
+      console.log("createUser:",createUser)
+      res.status(200).json({
+        status:true,
+        message:`employee register sucessfully with email:${createUser.email}`,
+        details:{name,email,role}
+      })
+    }
+  } catch (error) {
+    console.log("register error:",error.message)
+      res.status(200).json({
+        status:false,
+        message:`register error`,
+        error_message:error.message
+      })
+  }
 };
 // module exports
 module.exports = { registerController, loginController };
