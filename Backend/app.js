a42a64f9c90475f7b1c5407dccf27cae6d23f3e4
Madhari Tarunsai <saitarunsai04062002@gmail.com>
2026-05-22 02:03:55 +0530

    added

--- Backend/app.js
+++ Backend/app.js
@@ -6,11 +6,15 @@ const authRouter = require("./Routers/authRouter/authRouter.js");
 const employeeRouter = require("./Routers/createEmployeeRouter/createEmployeeRouter.js");
 const taskRoute = require("./Routers/taskRoutes/taskRoutes.js");
 const employeeTasks = require("./Routers/employeeTaskRoute/employeeTaskRoute.js");
+const cors=require('cors')
 const port = process.env.PORT || 8015;
 
 // built-in middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// added cors
+app.use(cors());
 
 // router middleware
 app.use("/api/auth", authRouter);
